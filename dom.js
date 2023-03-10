2357393c5ca60326d48919fece17fd33935282ed
saiashish8364 <polusaiashish97@gmail.com>
2023-03-11 00:31:13 +0530

    sixth commit

--- dom.js
+++ dom.js
@@ -1,4 +1,4 @@
-var headertitle=document.getElementById("main-header");
+/*var headertitle=document.getElementById("main-header");
 headertitle.textContent="List Of Items";
 var pagetitle=document.getElementById("title");
 pagetitle.textContent="Item Lister page";
@@ -15,14 +15,14 @@ for(var i=0;i<t.length;i++)
 }
 t[1].style.backgroundColor="blue";
 
-/**var thirditem=document.querySelector(
+var thirditem=document.querySelector(
     ".list-group-item:nth-child(3)"
 ).style.visibility = "hidden";
 
 var thirditem=document.querySelectorAll(
     "li:nth-child(even)"
 );
-thirditem[0].style.color="green";*/
+thirditem[0].style.color="green";
 
 var odd=document.querySelectorAll(
     "li:nth-child(odd)"
@@ -31,3 +31,35 @@ for(var i=0;i<odd.length;i++)
 {
     odd[i].style.backgroundColor="green";
 }
+*/
+
+var itemlist=document.querySelector("#items");
+itemlist.parentElement.style.backgroundColor="#f4f4f4";
+
+
+//child node
+
+itemlist.lastElementChild.textContent="hi";
+itemlist.firstElementChild.textContent="1st";
+itemlist.lastChild.textContent="changed last element";
+itemlist.firstChild.textContent="changed first element";
+itemlist.createchild="created";
+itemlist.nextSibling.textContent="nextsibiling";
+console.log(itemlist.previousElementSibling);
+console.log(itemlist.previousSibling);
+itemlist.previousElementSibling.style.color="yellow";
+
+var nd=document.createElement('div');
+nd.className='hello';
+nd.id="hello1";
+nd.setAttribute("title","hello div");
+var ndt=document.createTextNode("hello world");
+nd.appendChild(ndt);
+
+var container=document.querySelector("header .container");
+var h1=document.querySelector("header h1");
+nd.style.fontSize="30px";
+container.insertBefore(nd,h1);
+
+itemlist.firstChild.textContent="Hello World";
+itemlist.firstChild.fontWeight="15px";
