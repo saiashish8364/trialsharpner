d38cc155992d7430bc9bdf978b11f523459b4124
saiashish8364 <polusaiashish97@gmail.com>
2023-03-10 19:41:46 +0530

    third commit

--- dom.js
+++ dom.js
@@ -6,3 +6,11 @@ headertitle.style.borderBottom="solid 3px #000";
 var ai=document.getElementById("title1")
 ai.style.fontWeight="bold";
 ai.style.color="green";
+var t=document.getElementsByClassName("list-group-item");
+
+for(var i=0;i<t.length;i++)
+{
+    t[i].style.fontWeight="bold";
+    t[i].style.backgroundColor="#f4f4f4";
+}
+t[2].style.backgroundColor="green";
