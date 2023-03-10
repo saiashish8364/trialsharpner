4aa827a1dc5bf8e258efbf30a98c1a005f339855
saiashish8364 <polusaiashish97@gmail.com>
2023-03-10 20:29:26 +0530

    fifth commit

--- dom.js
+++ dom.js
@@ -13,4 +13,21 @@ for(var i=0;i<t.length;i++)
     t[i].style.fontWeight="bold";
     t[i].style.backgroundColor="#f4f4f4";
 }
-t[2].style.backgroundColor="green";
+t[1].style.backgroundColor="blue";
+
+/**var thirditem=document.querySelector(
+    ".list-group-item:nth-child(3)"
+).style.visibility = "hidden";
+
+var thirditem=document.querySelectorAll(
+    "li:nth-child(even)"
+);
+thirditem[0].style.color="green";*/
+
+var odd=document.querySelectorAll(
+    "li:nth-child(odd)"
+);
+for(var i=0;i<odd.length;i++)
+{
+    odd[i].style.backgroundColor="green";
+}
