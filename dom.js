8e5eba65d46324912cea57e9d3a9494c2c4ed030
saiashish8364 <polusaiashish97@gmail.com>
2023-03-10 19:10:40 +0530

    second commit

---
+++ dom.js
@@ -0,0 +1,8 @@
+var headertitle=document.getElementById("main-header");
+headertitle.textContent="List Of Items";
+var pagetitle=document.getElementById("title");
+pagetitle.textContent="Item Lister page";
+headertitle.style.borderBottom="solid 3px #000";
+var ai=document.getElementById("title1")
+ai.style.fontWeight="bold";
+ai.style.color="green";
