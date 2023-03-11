c2d1a3005f962a2b4dfc1607f03c2335c27b3724
saiashish8364 <polusaiashish97@gmail.com>
2023-03-11 18:57:02 +0530

    tenth commit

--- dom.js
+++ dom.js
@@ -67,13 +67,73 @@ itemlist.firstChild.fontWeight="15px";
 
 var form = document.getElementById('addForm');
 var itemList = document.getElementById('items');
+var filter = document.getElementById('filter');
 
+// Form submit event
+form.addEventListener('submit', addItem);
+// Delete event
 itemList.addEventListener('click', removeItem);
+// Filter event
+filter.addEventListener('keyup', filterItems);
+
+// Add item
+function addItem(e){
+  e.preventDefault();
+
+  
+  var newItem = document.getElementById('item').value;
+
+  
+  var li = document.createElement('li');
+ 
+  li.className = 'list-group-item';
+  
+  li.appendChild(document.createTextNode(newItem));
+
+  
+  var deleteBtn = document.createElement('button');
+  var editbtn=document.createElement("button");
+
+
+  
+  deleteBtn.className = 'btn btn-danger btn-sm float-right delete';
+  editbtn.className='btn btn-primary btn-sm float-right ';
+
+
+  deleteBtn.appendChild(document.createTextNode('X'));
+  editbtn.appendChild(document.createTextNode("Edit"));
+
+ 
+  li.appendChild(deleteBtn);
+  li.appendChild(editbtn);
+
+  
+  itemList.appendChild(li);
+}
+
+// Remove item
 function removeItem(e){
-    if(e.target.classList.contains('delete')){
-      if(confirm('Are You Sure?')){
-        var li = e.target.parentElement;
-        itemList.removeChild(li);
-      }
+  if(e.target.classList.contains('delete')){
+    if(confirm('Are You Sure?')){
+      var li = e.target.parentElement;
+      itemList.removeChild(li);
     }
   }
+}
+
+// Filter Items
+function filterItems(e){
+  
+  var text = e.target.value.toLowerCase();
+ 
+  var items = itemList.getElementsByTagName('li');
+  
+  Array.from(items).forEach(function(item){
+    var itemName = item.firstChild.textContent;
+    if(itemName.toLowerCase().indexOf(text) != -1){
+      item.style.display = 'block';
+    } else {
+      item.style.display = 'none';
+    }
+  });
+}
