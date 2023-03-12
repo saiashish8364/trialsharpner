edf43beff7eadd1d89de96e53303c63eb2e899b5
saiashish8364 <polusaiashish97@gmail.com>
2023-03-12 18:43:08 +0530

    fourteenth commit

--- rpage/reg.js
+++ rpage/reg.js
@@ -1,8 +1,9 @@
 const myform=document.querySelector("#my-form");
 
 myform.addEventListener("submit",onsubmit);
+
 function onsubmit(e){
-   // e.preventDefault();
+    e.preventDefault();
     var name=document.getElementById("name").value;
     var email=document.getElementById("email").value;
     var mobile=document.getElementById("mobile").value;
@@ -14,4 +15,23 @@ function onsubmit(e){
     };
     let myobjser=JSON.stringify(myobj);
     localStorage.setItem(myobj.Email,myobjser);
-}
+    showonscreen(myobj)
+}   
+   function showonscreen(myobj){
+    e.preventDefault();
+    const pe=document.getElementById("ulist")
+    const ce=document.createElement("li")
+    ce.textContent=myobj.Name+'-'+myobj.Email+'-'+myobj.Mobile
+    const db=document.createElement("input")
+    db.type="button"
+    db.value="delete"
+    db.onclick=()=>
+    {
+        localStorage.removeItem(myobj.Email)
+        pe.removeChild(ce)
+   }
+   ce.appendChild(db)
+   pe.appendChild(ce)
+
+
+   }
