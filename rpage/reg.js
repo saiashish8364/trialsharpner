83ce9cf34a78418176fb68e012a73560fa3c0d8f
saiashish8364 <polusaiashish97@gmail.com>
2023-03-13 15:13:57 +0530

    fifteenth commit

--- rpage/reg.js
+++ rpage/reg.js
@@ -18,19 +18,35 @@ function onsubmit(e){
     showonscreen(myobj)
 }   
    function showonscreen(myobj){
-    e.preventDefault();
+    
     const pe=document.getElementById("ulist")
     const ce=document.createElement("li")
     ce.textContent=myobj.Name+'-'+myobj.Email+'-'+myobj.Mobile
     const db=document.createElement("input")
+    const eb=document.createElement("input")
     db.type="button"
     db.value="delete"
+    eb.type="button"
+    eb.value="Edit"
     db.onclick=()=>
     {
         localStorage.removeItem(myobj.Email)
         pe.removeChild(ce)
    }
+   eb.onclick=()=>
+    {
+        var name1=myobj.Name
+        var email1=myobj.Email
+        var mobile1=myobj.Mobile
+        document.getElementById("name").value=name1
+        document.getElementById("email").value=email1
+        document.getElementById("mobile").value=mobile1
+
+        localStorage.removeItem(myobj.Email)
+        pe.removeChild(ce)
+   }
    ce.appendChild(db)
+   ce.appendChild(eb)
    pe.appendChild(ce)
 
 
