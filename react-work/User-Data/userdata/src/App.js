1626cc9375a93eef1b57c697e5c756d57139b8c2
saiashish8364 <polusaiashish97@gmail.com>
2023-05-04 14:35:47 +0530

    userup2

--- react-work/User-Data/userdata/src/App.js
+++ react-work/User-Data/userdata/src/App.js
@@ -1,7 +1,7 @@
-import React, { useState } from 'react';
+import React, { useState, Fragment } from "react";
 
-import AddUser from './components/Users/AddUser';
-import UsersList from './components/Users/UsersList';
+import AddUser from "./components/Users/AddUser";
+import UsersList from "./components/Users/UsersList";
 
 function App() {
   const [usersList, setUsersList] = useState([]);
@@ -16,10 +16,14 @@ function App() {
   };
 
   return (
-    <div>
+    //<>
+    //<React.Fragment>
+    <Fragment>
       <AddUser onAddUser={addUserHandler} />
       <UsersList users={usersList} />
-    </div>
+    </Fragment>
+    //</React.Fragment>
+    //</>
   );
 }
 
