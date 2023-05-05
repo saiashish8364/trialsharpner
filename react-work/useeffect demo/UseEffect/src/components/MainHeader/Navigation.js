a86f3d5d11ba244751d293aecd149d153d55c960
saiashish8364 <polusaiashish97@gmail.com>
2023-05-05 18:27:25 +0530

    ue4-context

--- react-work/useeffect demo/UseEffect/src/components/MainHeader/Navigation.js
+++ react-work/useeffect demo/UseEffect/src/components/MainHeader/Navigation.js
@@ -1,10 +1,11 @@
-import React, { useContext } from "react";
+import React, { useContext } from 'react';
 
-import classes from "./Navigation.module.css";
-import AuthContext from "../../context/auth-context";
+import AuthContext from '../../store/auth-context';
+import classes from './Navigation.module.css';
 
 const Navigation = () => {
   const ctx = useContext(AuthContext);
+
   return (
     <nav className={classes.nav}>
       <ul>
--- react-work/useeffect demo/UseEffect/src/context/auth-context.js
+++ react-work/useeffect demo/UseEffect/src/context/auth-context.js
@@ -1,5 +1,35 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 const AuthContext = React.createContext({
   isLoggedIn: false,
+  onLogout: () => {},
+  onLogin: (email, password) => {},
 });
+export const AuthContextProvider = (props) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  useEffect(() => {
+    const storedUserLoggedInInformatioin = localStorage.getItem("isLoggedIn");
+    if (storedUserLoggedInInformatioin === "1") {
+      setIsLoggedIn(true);
+    }
+  }, []);
+  const logoutHandler = () => {
+    localStorage.removeItem("isLoggedIn");
+    setIsLoggedIn(false);
+  };
+  const loginHandler = () => {
+    localStorage.setItem("isLoggedIn", "1");
+    setIsLoggedIn(true);
+  };
+  return (
+    <AuthContext.Provider
+      value={{
+        isLoggedIn: isLoggedIn,
+        onLogout: logoutHandler,
+        onLogin: loginHandler,
+      }}
+    >
+      {props.childern}
+    </AuthContext.Provider>
+  );
+};
 export default AuthContext;
